b638a58015b6227c086e5884b799b68c22f1c2af
nomus62 <40475923+nomus62@users.noreply.github.com>
2026-08-28 15:23:58 +0500

    add update all data:
    add update

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -171,9 +171,3 @@ public class ChatController : ControllerBase
     }
 }
 
-public class SendMessageDto
-{
-    public int ReceiverId { get; set; }
-    public string Content { get; set; } = string.Empty;
-}
-
